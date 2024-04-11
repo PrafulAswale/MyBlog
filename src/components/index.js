95461be7b39a1948f34e82c1e0a71695dbca2d69
Praful Aswale <prafulcode15@gmail.com>
2024-04-11 23:31:27 +0530

    components

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,23 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import Select from "./Select";
+import Signup from "./Signup";
 
-export { Header, Footer, Logo, Container, LogoutBtn };
+export {
+  Header,
+  Footer,
+  Logo,
+  Container,
+  LogoutBtn,
+  Button,
+  Input,
+  Login,
+  PostCard,
+  Select,
+  Signup,
+};
