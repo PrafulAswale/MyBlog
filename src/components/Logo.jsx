4e27f83721732a126c308574b19f6b3bf9021dc2
Praful Aswale <prafulcode15@gmail.com>
2024-04-14 10:34:36 +0530

    logo added

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import myBlog from "../assets/my-blog-logo.png";
 
 function Logo({ width = "100px" }) {
-  return <div>Logo</div>;
+  return <img src={myBlog} alt="My Blog" srcSet="" width={width} />;
 }
 
 export default Logo;
